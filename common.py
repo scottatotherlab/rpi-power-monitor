77c0e790e00f336d16030728a0292d3961b0a99b
Scott Brandenburg <scott@otherlab.com>
2022-03-09 17:24:08 -0800

    Merge branch 'master' of https://github.com/scottatotherlab/rpi-power-monitor

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 # This module contains functions that are used in both the main power-monitor code and the calibration code.
 
 from datetime import datetime
-from config import ct_phase_correction, ct0_channel, ct1_channel, ct2_channel, ct3_channel, ct4_channel, board_voltage_channel, v_sensor_channel, ct5_channel, logger
+from config import ct_phase_correction, ct0_channel, ct1_channel, ct2_channel, ct3_channel, ct4_channel, board_voltage_channel, v_sensor_channel, vbatt_sensor_channel, logger
 import spidev
 import subprocess
 import docker
@@ -30,24 +30,24 @@ def collect_data(numSamples):
     ct2_data = []
     ct3_data = []
     ct4_data = []
-    ct5_data = []
     v_data = []
+    vbatt_data = []
 
     for _ in range(numSamples):
         ct0 = readadc(ct0_channel)
-        ct4 = readadc(ct4_channel)
         ct1 = readadc(ct1_channel)
-        v = readadc(v_sensor_channel)
         ct2 = readadc(ct2_channel)
         ct3 = readadc(ct3_channel)
-        ct5 = readadc(ct5_channel)
+        ct4 = readadc(ct4_channel)
+        v = readadc(v_sensor_channel)
+        vbatt = readadc(vbatt_sensor_channel)
         ct0_data.append(ct0)
         ct1_data.append(ct1)
         ct2_data.append(ct2)
         ct3_data.append(ct3)
         ct4_data.append(ct4)
-        ct5_data.append(ct5)
         v_data.append(v)    
+        vbatt_data.append(vbatt)
     
     samples = {
         'ct0' : ct0_data,
@@ -55,8 +55,8 @@ def collect_data(numSamples):
         'ct2' : ct2_data,
         'ct3' : ct3_data,
         'ct4' : ct4_data,
-        'ct5' : ct5_data,
-        'voltage' : v_data,
+        'AC voltage' : v_data,
+        'DC voltage' : vbatt_data,
         'time' : now,
     }
     return samples
--- config.py
+++ config.py
@@ -16,6 +16,10 @@ GRID_VOLTAGE = 120.7 # 124.2
 # Using a multimeter, measure the output voltage of your AC transformer. Using the value on the label is not ideal and will lead to greater accuracy in the calculations.
 AC_TRANSFORMER_OUTPUT_VOLTAGE = 10.3 # 10.2
 
+BATT_VOLTAGE = 10.83
+
+DC_VOLTAGE_DIVIDER_OUTPUT_VOLTAGE = 2.75
+
 # InfluxDB Settings
 db_settings = {
     'host' : 'localhost',
@@ -34,25 +38,24 @@ ct3_channel = 3             # Brown Pair            | Solar Power
 ct4_channel = 6             # 3.5mm Input #1        | Subpanel main (leg 2 - bottom)
 board_voltage_channel =  4  # Board voltage ~3.3V
 v_sensor_channel = 5        # 9V AC Voltage channel
-ct5_channel = 7             # 3.5mm Input #2        | Unused
+vbatt_sensor_channel = 7    # battery output voltage channel
 
 # The values from running the software in "phase" mode should go below!
 ct_phase_correction = {
-    'ct0' : 1.71340443, # 
+    'ct0' : 1.71340443,
     'ct1' : 1,
     'ct2' : 1,
     'ct3' : 1,
     'ct4' : 1,
-    'ct5' : 1,
 }
 
 # AFTER phase correction is completed, these values are used in the final calibration for accuracy. See the documentation for more information.
 accuracy_calibration = {
-    'ct0' : 0.16819, # 1
+    'ct0' : 0.16819,
     'ct1' : 1,
     'ct2' : 1,
     'ct3' : 1,
     'ct4' : 1,
-    'ct5' : 1,
-    'AC'  : 1.07346, # 1
+    'AC'  : 1.07346,
+    'DC'  : 1, # battery output voltage channel calibration 
 }
